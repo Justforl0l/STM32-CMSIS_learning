8aeddd620eaba2a629f09f7114a895631d895bd2
Just_for_LoLz <just_for_lolz@yahoo.com>
2022-07-03 16:42:37 +0400

    Реализовал функцию led_toggle

--- Blinking_LED/src/main.cpp
+++ Blinking_LED/src/main.cpp
@@ -13,7 +13,14 @@ int main()
 
 void led_toggle()
 {
-    return;
+    if (GPIOC->ODR & (1 << 13))
+    {
+        GPIOC->BRR = GPIOC->BRR | (1 << 13);
+    }
+    else
+    {
+        GPIOC->BSRR = GPIOC->BSRR | (1 << 13);
+    }
 }
 
 void delay(uint8_t delay_ms)
