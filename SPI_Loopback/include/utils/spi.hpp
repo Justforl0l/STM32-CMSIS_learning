d6b38df38211d637a4a6bb3788268e70ba4290d6
Just_for_LoLz <just_for_lolz@yahoo.com>
2022-07-13 13:38:53 +0400

    Добавил шаблон функций для взаимодействия с SPI

---
+++ SPI_Loopback/include/utils/spi.hpp
@@ -0,0 +1,6 @@
+#pragma once
+
+#include "stm32f103xb.h"
+
+void SPI_SendData(SPI_TypeDef *SPIx, uint8_t *TxBuffer, int count);
+void SPI_RecieveData(SPI_TypeDef *SPIx, uint8_t *RxBuffer);
---
+++ SPI_Loopback/src/utils/spi.cpp
@@ -0,0 +1,11 @@
+#include "utils/spi.hpp"
+
+void SPI_SendData(SPI_TypeDef *SPIx, uint8_t *TxBuffer, int count)
+{
+    __NOP();
+}
+
+void SPI_RecieveData(SPI_TypeDef *SPIx, uint8_t *RxBuffer)
+{
+    __NOP();
+}
