c45b69d17d6b7dda9134c95823ad2100de521abe
Just_for_LoLz <just_for_lolz@yahoo.com>
2022-07-19 16:18:17 +0400

    Реализовал функцию SPI_SendRecieveData16 для передачи и приема данных в 16-ти битном режиме

--- SPI_Loopback/include/utils/spi.hpp
+++ SPI_Loopback/include/utils/spi.hpp
@@ -4,3 +4,5 @@
 
 void SPI_SendRecieveData(SPI_TypeDef *SPIx, uint8_t *TxBuffer,
                          uint8_t *RxBuffer, int count);
+void SPI_SendRecieveData16(SPI_TypeDef *SPIx, uint16_t *TxBuffer16,
+                           uint16_t *RxBuffer16, int count);
--- SPI_Loopback/src/utils/spi.cpp
+++ SPI_Loopback/src/utils/spi.cpp
@@ -25,3 +25,31 @@ void SPI_SendRecieveData(SPI_TypeDef *SPIx, uint8_t *TxBuffer,
         }
     }
 }
+
+void SPI_SendRecieveData16(SPI_TypeDef *SPIx, uint16_t *TxBuffer16,
+                           uint16_t *RxBuffer16, int count)
+{
+    SPIx->CR1 |= SPI_CR1_DFF;
+    for (int i = 0; i < count; i++)
+    {
+        while (!(SPIx->SR & SPI_SR_TXE_Msk));
+        if (TxBuffer16)
+        {
+            SPIx->DR = *TxBuffer16++;
+        }
+        else
+        {
+            SPIx->DR = 0xFF;
+        }
+        while (!(SPIx->SR & SPI_SR_RXNE_Msk));
+        if (RxBuffer16)
+        {
+            *RxBuffer16++ = SPIx->DR;
+        }
+        else
+        {
+            SPIx->DR;
+        }
+    }
+    SPIx->CR1 &= ~SPI_CR1_DFF;
+}
