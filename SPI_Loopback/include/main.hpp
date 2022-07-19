eee7454188aa2372c24dc28ad36b81f9d34567c4
Just_for_LoLz <just_for_lolz@yahoo.com>
2022-07-19 16:19:31 +0400

    Закончил проект SPI_Loopback
    - Добавил функцию assert_failed для проверки транзакций
    - Реализовал наполнение и передачу 16-ти битных данных
    - Реализовал проверку 8-ми и 16-ти битных данных после транзакции

--- SPI_Loopback/include/main.hpp
+++ SPI_Loopback/include/main.hpp
@@ -4,3 +4,5 @@
 
 #include "init.hpp"
 #include "utils/spi.hpp"
+
+void assert_failed(uint8_t *file, uint32_t line);
--- SPI_Loopback/src/main.cpp
+++ SPI_Loopback/src/main.cpp
@@ -1,6 +1,7 @@
 #include "main.hpp"
 
 uint8_t TxBuffer[4], RxBuffer[4];
+uint16_t TxBuffer16[4], RxBuffer16[4];
 
 int main()
 {
@@ -16,7 +17,39 @@ int main()
             {
                 TxBuffer[j] = i * 4 + j;
             }
+            GPIOB->BRR |= GPIO_BRR_BR10;
             SPI_SendRecieveData(SPI2, TxBuffer, RxBuffer, 4);
+            GPIOB->BSRR |= GPIO_BSRR_BS10;
+            for (j = 0; j < 4; j++)
+            {
+                if (RxBuffer[j] != TxBuffer[j])
+                {
+                    assert_failed((uint8_t *)__FILE__, __LINE__);
+                }
+            }
+        }
+
+        for (i = 0; i < 8; i++)
+        {
+            for (j = 0; j < 4; j++)
+            {
+                TxBuffer16[j] = i * 4 + j + (i << 8);
+            }
+            GPIOB->BRR |= GPIO_BRR_BR10;
+            SPI_SendRecieveData16(SPI2, TxBuffer16, RxBuffer16, 4);
+            GPIOB->BSRR |= GPIO_BSRR_BS10;
+            for (j = 0; j < 4; j++)
+            {
+                if (RxBuffer16[j] != TxBuffer16[j])
+                {
+                    assert_failed((uint8_t *)__FILE__, __LINE__);
+                }
+            }
         }
     }
+}
+
+void assert_failed(uint8_t *file, uint32_t line)
+{
+    while (1);
 }
