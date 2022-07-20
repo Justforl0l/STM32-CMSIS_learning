198302396d385aaacad8ddbef918983ba67f1fc4
Just_for_LoLz <just_for_lolz@yahoo.com>
2022-07-20 21:35:29 +0400

    Реализовал функцию clock_init

--- SPI_TFTFillScreen/src/init.cpp
+++ SPI_TFTFillScreen/src/init.cpp
@@ -9,7 +9,15 @@ void mcu_init()
 
 void clock_init()
 {
-    __NOP();
+    RCC->CR |= RCC_CR_HSEON;
+    while (!(RCC->CR & RCC_CR_HSERDY_Msk));
+    RCC->CFGR |= (RCC_CFGR_PLLMULL9 | RCC_CFGR_PLLSRC);
+    RCC->CR |= RCC_CR_PLLON;
+    while (!(RCC->CR & RCC_CR_PLLRDY_Msk));
+    FLASH->ACR |= FLASH_ACR_LATENCY_2;
+    RCC->CFGR |= RCC_CFGR_PPRE1_DIV2 | RCC_CFGR_SW_PLL;
+    while (!(RCC->CFGR & RCC_CFGR_SWS_PLL));
+    RCC->CR &= ~RCC_CR_HSION;
 }
 
 void gpio_init()
