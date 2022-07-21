7bead8ae36b6122188051ad2d9028bf5599789ee
Just_for_LoLz <just_for_lolz@yahoo.com>
2022-07-21 10:29:10 +0400

    Описал основную логику модуля main
    - Добавил шаблон функции ST7735_init
    - Добавил шаблон функции sendCommand
    - Добавил шаблон функции ST7735_backlight
    - Добавил шаблон функции fillScreen
    - Добавил шаблон функции ST7735_pushColor
    - Добавил шаблон функции delay
    - Реализовал функцию SysTick_Handler

--- SPI_TFTFillScreen/include/main.hpp
+++ SPI_TFTFillScreen/include/main.hpp
@@ -3,6 +3,8 @@
 #include "stm32f103xb.h"
 
 #include "init.hpp"
+#include "tft/commands.hpp"
+#include "tft/colors.hpp"
 
 #define TFT_PORT_BLK        GPIOA
 #define TFT_PORT_RST        GPIOA
@@ -17,3 +19,24 @@
 
 #define ST7735_WIDTH        128
 #define ST7735_HEIGHT       160
+
+volatile uint32_t TICK = 0;
+
+void ST7735_init(SPI_TypeDef *SPIx, const uint8_t *commandList);
+void sendCommand(SPI_TypeDef *SPIx, uint8_t command,
+                 const uint8_t *address, uint8_t numArgs);
+void ST7735_backlight(uint8_t on);
+void fillScreen(SPI_TypeDef *SPIx, uint16_t color);
+void ST7735_pushColor(SPI_TypeDef *SPIx, uint16_t color, int count);
+void delay(uint32_t delay_ms);
+
+#ifdef __cplusplus
+extern "C"
+{
+    #endif // __cplusplus
+
+    void SysTick_Handler();
+
+    #ifdef __cplusplus
+}
+#endif // __cplusplus
--- SPI_TFTFillScreen/src/main.cpp
+++ SPI_TFTFillScreen/src/main.cpp
@@ -2,8 +2,99 @@
 
 int main()
 {
+    mcu_init();
+
+    static const uint8_t __attribute__((section(".rodata.ST7735S_InitCommandList"), used))
+        commandList[] = {
+            19,
+            ST7735S_CMD_SWRESET,    ST7735S_CMD_DELAY,
+                120,
+            ST7735S_CMD_SLPOUT,     ST7735S_CMD_DELAY,
+                255,
+            ST7735S_CMD_FRMCTR1,    3,
+                0x01, 0x2C, 0x2D,
+            ST7735S_CMD_FRMCTR2,    3,
+                0x01, 0x2C, 0x2D,
+            ST7735S_CMD_FRMCTR3,    6,
+                0x01, 0x2C, 0x2D,
+                0x01, 0x2C, 0x2D,
+            ST7735S_CMD_INVCTR,     1,
+                0x07,
+            ST7735S_CMD_PWRCTR1,    3,
+                0xA2, 0x02, 0x84,
+            ST7735S_CMD_PWRCTR2,    1,
+                0xC5,
+            ST7735S_CMD_PWRCTR3,    2,
+                0x0A, 0x00,
+            ST7735S_CMD_PWRCTR4,    2,
+                0x8A, 0x2A,
+            ST7735S_CMD_PWRCTR5,    2,
+                0x8A, 0xEE,
+            ST7735S_CMD_VMCTR1,     1,
+                0x0E,
+            ST7735S_CMD_INVOFF,     0,
+            ST7735S_CMD_MADCTL,     1,
+                0x08,
+            ST7735S_CMD_COLMOD,     1,
+                0x05,
+            ST7735S_CMD_CASET,      4,
+                0x00, 0x00,
+                0x00, 0x7F,
+            ST7735S_CMD_RASET,      4,
+                0x00, 0x00,
+                0x00, 0x9F,
+            ST7735S_CMD_NORON,      ST7735S_CMD_DELAY,
+                10,
+            ST7735S_CMD_DISPON,     ST7735S_CMD_DELAY,
+                255
+        };
+
+    ST7735_init(SPI2, commandList);
+    ST7735_backlight(1);
+
     while (1)
     {
-        __NOP();
+        fillScreen(SPI2, _RED);
+        delay(1000);
+        fillScreen(SPI2, _GREEN);
+        delay(1000);
+        fillScreen(SPI2, _BLUE);
+        delay(1000);
     }
+}
+
+void SysTick_Handler()
+{
+    TICK++;
+}
+
+void ST7735_init(SPI_TypeDef *SPIx, const uint8_t *commandList)
+{
+    __NOP();
+}
+
+void sendCommand(SPI_TypeDef *SPIx, uint8_t command,
+                 const uint8_t *address, uint8_t numArgs)
+{
+    __NOP();
+}
+
+void ST7735_backlight(uint8_t on)
+{
+    __NOP();
+}
+
+void fillScreen(SPI_TypeDef *SPIx, uint16_t color)
+{
+    __NOP();
+}
+
+void ST7735_pushColor(SPI_TypeDef *SPIx, uint16_t color, int count)
+{
+    __NOP();
+}
+
+void delay(uint32_t delay_ms)
+{
+    __NOP();
 }
