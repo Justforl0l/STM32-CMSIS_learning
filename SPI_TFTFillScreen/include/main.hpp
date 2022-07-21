91419a4f57996acaf18530d857e7e72cf3dedacb
Just_for_LoLz <just_for_lolz@yahoo.com>
2022-07-21 09:41:03 +0400

    Добавил дефайны управляющих пинов дисплея, константы

--- SPI_TFTFillScreen/include/main.hpp
+++ SPI_TFTFillScreen/include/main.hpp
@@ -3,3 +3,17 @@
 #include "stm32f103xb.h"
 
 #include "init.hpp"
+
+#define TFT_PORT_BLK        GPIOA
+#define TFT_PORT_RST        GPIOA
+#define TFT_PORT_DC         GPIOA
+#define TFT_PORT_SCE        GPIOA
+#define TFT_PORT_MISO       GPIOB
+#define TFT_PORT_MOSI       GPIOB
+#define TFT_PORT_CLK        GPIOB
+
+#define MADCTLGRAPHICS      0x6
+#define MADCTLBMP           0x2
+
+#define ST7735_WIDTH        128
+#define ST7735_HEIGHT       160
