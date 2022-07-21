904be32266bc5c29a9eb1b7bcacbc7e4618996b8
Just_for_LoLz <just_for_lolz@yahoo.com>
2022-07-21 09:30:46 +0400

    Реализовал функцию SPI_SendData

--- SPI_TFTFillScreen/src/utils/spi.cpp
+++ SPI_TFTFillScreen/src/utils/spi.cpp
@@ -2,5 +2,17 @@
 
 void SPI_SendData(SPI_TypeDef *SPIx, uint8_t *TxBuffer, int count)
 {
-    __NOP();
+    while (count != 0)
+    {
+        while (!(SPIx->SR & SPI_SR_TXE_Msk));
+        if (TxBuffer)
+        {
+            SPIx->DR = *TxBuffer++;
+        }
+        else
+        {
+            SPIx->DR = 0xFF;
+        }
+        count--;
+    }
 }
