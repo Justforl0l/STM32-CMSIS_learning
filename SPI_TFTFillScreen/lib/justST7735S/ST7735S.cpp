3b888ea10225f8adb1269edd4e1fac6013b9d7e4
Just_for_LoLz <just_for_lolz@yahoo.com>
2022-07-23 10:35:28 +0400

    Описал класс JST7735S

--- SPI_TFTFillScreen/lib/justST7735S/ST7735S.cpp
+++ SPI_TFTFillScreen/lib/justST7735S/ST7735S.cpp
@@ -1,1 +1,32 @@
 #include <inc/ST7735S.hpp>
+
+JST7735S::JST7735S(SPI_TypeDef *SPIx)
+{
+    __NOP();
+}
+
+void JST7735S::JST7735S_initDisplay(const uint8_t *commandList)
+{
+    __NOP();
+}
+
+void JST7735S::JST7735S_sendCommand(uint8_t command, const uint8_t *address,
+                                    uint8_t numArgs)
+{
+    __NOP();
+}
+
+void JST7735S::JST7735S_backlight(bool on)
+{
+    __NOP();
+}
+
+void JST7735S::JST7735S_fillScreen(uint16_t color)
+{
+    __NOP();
+}
+
+void JST7735S::JST7735S_pushColor(uint16_t color, int count)
+{
+    __NOP();
+}
--- SPI_TFTFillScreen/lib/justST7735S/inc/ST7735S.hpp
+++ SPI_TFTFillScreen/lib/justST7735S/inc/ST7735S.hpp
@@ -3,15 +3,21 @@
 #include "colors.hpp"
 #include "commands.hpp"
 
-#ifdef STM32F103xB
 #include "stm32f103xb.h"
-#endif  // STM32F103xB
 
 #include "utils/spi.hpp"
 
 class JST7735S
 {
     private:
+        SPI_TypeDef *_SPI;
     public:
+        JST7735S(SPI_TypeDef *SPIx);
+        void JST7735S_initDisplay(const uint8_t *commandList);
+        void JST7735S_sendCommand(uint8_t command, const uint8_t *address,
+                             uint8_t numArgs);
+        void JST7735S_backlight(bool on);
+        void JST7735S_fillScreen(uint16_t color);
+        void JST7735S_pushColor(uint16_t color, int count);
     protected:
 };
