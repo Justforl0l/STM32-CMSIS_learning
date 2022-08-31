a7c12aac875cedbe39db3d72a82d4c92d1ac2e75
Just_for_LoLz <just_for_lolz@yahoo.com>
2022-08-31 13:27:19 +0400

    Продолжил реализацию класса JST7735S
    - Изменил сигнатуру некоторых функций
    - Добавил приватный атрибут класса
    - Убрал из названий методов название класса (sic :))

--- SPI_TFTFillScreen/lib/justST7735S/ST7735S.cpp
+++ SPI_TFTFillScreen/lib/justST7735S/ST7735S.cpp
@@ -1,32 +1,33 @@
 #include <inc/ST7735S.hpp>
 
-JST7735S::JST7735S(SPI_TypeDef *SPIx)
+JST7735S::JST7735S(SPI_TypeDef *SPIx, const uint8_t *commandList)
+{
+    this->_SPI = SPIx;
+    this->_commandList = commandList;
+}
+
+void JST7735S::initDisplay()
 {
     __NOP();
 }
 
-void JST7735S::JST7735S_initDisplay(const uint8_t *commandList)
+void JST7735S::sendCommand(uint8_t command, const uint8_t *address,
+                           uint8_t numArgs)
 {
     __NOP();
 }
 
-void JST7735S::JST7735S_sendCommand(uint8_t command, const uint8_t *address,
-                                    uint8_t numArgs)
+void JST7735S::toggleBacklight()
 {
     __NOP();
 }
 
-void JST7735S::JST7735S_backlight(bool on)
+void JST7735S::fillScreen(uint16_t color)
 {
     __NOP();
 }
 
-void JST7735S::JST7735S_fillScreen(uint16_t color)
-{
-    __NOP();
-}
-
-void JST7735S::JST7735S_pushColor(uint16_t color, int count)
+void JST7735S::pushColor(uint16_t color, int count)
 {
     __NOP();
 }
--- SPI_TFTFillScreen/lib/justST7735S/inc/ST7735S.hpp
+++ SPI_TFTFillScreen/lib/justST7735S/inc/ST7735S.hpp
@@ -11,13 +11,14 @@ class JST7735S
 {
     private:
         SPI_TypeDef *_SPI;
+        const uint8_t *_commandList;
     public:
-        JST7735S(SPI_TypeDef *SPIx);
-        void JST7735S_initDisplay(const uint8_t *commandList);
-        void JST7735S_sendCommand(uint8_t command, const uint8_t *address,
+        JST7735S(SPI_TypeDef *SPIx, const uint8_t *commandList);
+        void initDisplay();
+        void sendCommand(uint8_t command, const uint8_t *address,
                              uint8_t numArgs);
-        void JST7735S_backlight(bool on);
-        void JST7735S_fillScreen(uint16_t color);
-        void JST7735S_pushColor(uint16_t color, int count);
+        void toggleBacklight();
+        void fillScreen(uint16_t color);
+        void pushColor(uint16_t color, int count);
     protected:
 };
