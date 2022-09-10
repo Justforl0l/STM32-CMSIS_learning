6e7600f6ca729ef359b03c79500787f266d9fbc8
Just_for_LoLz <just_for_lolz@yahoo.com>
2022-09-11 01:09:53 +0400

    Изменил объявление методов

--- SPI_TFTFillScreen/lib/justST7735S/inc/TFTInterface.hpp
+++ SPI_TFTFillScreen/lib/justST7735S/inc/TFTInterface.hpp
@@ -4,14 +4,14 @@ class TFTInterface
 {
     public:
         void operator=(const TFTInterface&) = delete;
-        virtual inline void setCommandMode() = 0;
-        virtual inline void setDataMode() = 0;
-        virtual inline void waitUntilDataIsSent() = 0;
-        virtual inline void toggleBacklight() = 0;
-        virtual inline void selectDisplay() = 0;
-        virtual inline void deselectDisplay() = 0;
-        virtual inline void resetDisplay() = 0;
-        virtual inline void enableDisplay() = 0;
+        virtual void setCommandMode() = 0;
+        virtual void setDataMode() = 0;
+        virtual void waitUntilDataIsSent() = 0;
+        virtual void toggleBacklight() = 0;
+        virtual void selectDisplay() = 0;
+        virtual void deselectDisplay() = 0;
+        virtual void resetDisplay() = 0;
+        virtual void enableDisplay() = 0;
 
     protected:
         TFTInterface() = default;
