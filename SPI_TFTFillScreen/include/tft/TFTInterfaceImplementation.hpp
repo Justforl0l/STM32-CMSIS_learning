1fd6c3c26e930519c506fc164100b0c85eb6f49b
Just_for_LoLz <just_for_lolz@yahoo.com>
2022-09-11 12:27:33 +0400

    Рефакторинг класса TFTInterfaceImplementation

--- SPI_TFTFillScreen/include/tft/TFTInterfaceImplementation.hpp
+++ SPI_TFTFillScreen/include/tft/TFTInterfaceImplementation.hpp
@@ -24,3 +24,48 @@ class TFTInterfaceImplementation : public TFTInterface
     protected:
         ~TFTInterfaceImplementation() = default;
 };
+
+TFTInterfaceImplementation::TFTInterfaceImplementation(SPI_TypeDef* SPIx)
+{
+    _SPI = SPIx;
+}
+
+inline void TFTInterfaceImplementation::setCommandMode()
+{
+    TFT_PORT_DC->BRR |= TFT_PIN_DC;
+}
+
+inline void TFTInterfaceImplementation::setDataMode()
+{
+    TFT_PORT_DC->BSRR |= TFT_PIN_DC;
+}
+
+inline void TFTInterfaceImplementation::waitUntilDataIsSent()
+{
+    while (_SPI->SR & SPI_SR_BSY_Msk);
+}
+
+inline void TFTInterfaceImplementation::toggleBacklight()
+{
+    TFT_PORT_BLK->BSRR ^= TFT_PIN_BLK_BSRR_BS1;
+}
+
+inline void TFTInterfaceImplementation::selectDisplay()
+{
+    TFT_PORT_SCE->BRR |= TFT_PIN_SCE;
+}
+
+inline void TFTInterfaceImplementation::deselectDisplay()
+{
+    TFT_PORT_SCE->BSRR |= TFT_PIN_SCE;
+}
+
+inline void TFTInterfaceImplementation::resetDisplay()
+{
+    TFT_PORT_RST->BRR |= TFT_PIN_RST;
+}
+
+inline void TFTInterfaceImplementation::enableDisplay()
+{
+    TFT_PORT_RST->BSRR |= TFT_PIN_RST;
+}
